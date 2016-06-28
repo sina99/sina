aa6b70699709906d716879b2cb73cff058ee9ca5
sina99 <sina1378s@gmail.com>
2016-06-28 12:55:45 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {79356252,219322624,tonumber(our_id)},--Sudo users
+    sudo_users = {79356252},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
